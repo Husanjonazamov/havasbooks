89c514fb44b7c4782a9e38d23e1d3211396cd9ce
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-08 17:59:34 +0500

    "Cart serializer va URL'larni yangilash. User ma'lumotlarini qo'shish va cart item'larni ro'yxatga olish."

--- core/apps/havasbook/serializers/cart/cart.py
+++ core/apps/havasbook/serializers/cart/cart.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
-
+from core.apps.havasbook.serializers.cart.cartItem import ListCartitemSerializer
 from ...models import CartModel
 
 
 class BaseCartSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
     class Meta:
         model = CartModel
-        exclude = [
-            "created_at",
-            "updated_at",
+        fields = [
+            'id',
+            'user',
         ]
+        
+    def get_user(self, obj):
+        from core.apps.accounts.serializers import UserSerializer
+        return UserSerializer(obj.user).data
 
 
 class ListCartSerializer(BaseCartSerializer):
@@ -21,4 +26,8 @@ class RetrieveCartSerializer(BaseCartSerializer):
 
 
 class CreateCartSerializer(BaseCartSerializer):
+    cart_items = serializers.ListField(
+        child=ListCartitemSerializer(),
+        required=True
+    )
     class Meta(BaseCartSerializer.Meta): ...
--- core/apps/havasbook/urls.py
+++ core/apps/havasbook/urls.py
@@ -3,13 +3,18 @@ from rest_framework.routers import DefaultRouter
 from core.apps.havasbook.views import (
     BannerView,
     CategoryView,
-    BookView
+    BookView,
+    CartitemView,
+    CartView
 )
 
 router = DefaultRouter()
 router.register(r"banner", BannerView, basename='banner')
 router.register(r"category", CategoryView, basename='category')
 router.register(r"books", BookView, basename='books')
+router.register(r"cart", CartView, basename='cart')
+router.register(r"cart-item", CartitemView, basename='cart-item')
+
 
 
 
