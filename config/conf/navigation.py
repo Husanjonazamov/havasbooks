e35f6922297467696e3b12a7de68b45c870a90a5
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-08 11:59:51 +0500

    Foydalanuvchi guruhlarini va ruxsatlarni tekshirish funksiyasi qo'shildi

--- config/conf/navigation.py
+++ config/conf/navigation.py
@@ -1,6 +1,19 @@
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
+
+def user_has_group_or_permission(user, permission):
+    if user.is_superuser:
+        return True
+
+    group_names = user.groups.values_list("name", flat=True)
+    if not group_names:
+        return True
+
+    return user.groups.filter(permissions__codename=permission).exists()
+
+
+
 PAGES = [
     {
         "seperator": False,
@@ -17,15 +30,21 @@ PAGES = [
         "separator": True,  # Top border
         "items": [
             {
-                "title": _("Users"),
-                "icon": "group",
-                "link": reverse_lazy("admin:http_user_changelist"),
-            },
-            {
-                "title": _("Group"),
-                "icon": "group",
+                "title": _("Guruhlar"),
+                "icon": "groups_2",
                 "link": reverse_lazy("admin:auth_group_changelist"),
+                "permission": lambda request: user_has_group_or_permission(
+                    request.user, "view_group"
+                ),
             },
+            # {
+            #     "title": _("Foydalanuvchilar"),
+            #     "icon": "person_add",
+            #     "link": reverse_lazy("admin:auth_user_changelist"),
+            #     "permission": lambda request: user_has_group_or_permission(
+            #         request.user, "view_user"
+            #     ),
+            # },
         ],
     },
 ]
