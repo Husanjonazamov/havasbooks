6b9d0cc649bef96c820ed81b3310e8445ab968e2
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-08 15:14:11 +0500

    ` "Ijtimoiy tarmoqda foydalanuvchi jinsini qo'shish va eski migratsiyani o'chirish"`

--- core/apps/havasbook/models/order.py
+++ core/apps/havasbook/models/order.py
@@ -1,13 +1,56 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_core.models import AbstractBaseModel
+from core.apps.accounts.models import User
+
+
+
+class DeliveryMethodChoice(models.TextChoices):
+    DOOR_DELIVERY = "door_delivery", _("Eski uyga (Toshkent bo'ylab kur'er orqali yetkazib berish) 20.000~35.000 so'm")
+    PICKUP_POINT = "pickup_point", _("Yetkazib berish nuqtasiga (BTS pochta orqali O'zbekiston bo'ylab) 25.000~50.000 so'm")
+
+
+class PaymentMethodChoice(models.TextChoices):
+    CLICK = "click", _("Click")
+    PAYME = "payme", _("Payme")
+    PAYNET = "paynet", _("Paynet")
+    UZUM = "uzum_card", _("Uzum karta")
+
 
 
 class OrderModel(AbstractBaseModel):
-    name = models.CharField(_("name"), max_length=255)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+    location = models.ForeignKey(
+        "havasbook.LocationModel",
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+    delivery_method = models.CharField(
+        max_length=255,
+        choices=DeliveryMethodChoice.choices
+    )
+    payment_method = models.CharField(
+        _("To'lov turi"),
+        max_length=50,
+        choices=PaymentMethodChoice.choices,
+        default='click'
+    )
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
+    status = models.CharField(_("Status"), max_length=50, default='pending')  
+    comment = models.TextField(_("Buyrtma uchun izoh"), null=True, blank=True) 
+    
 
     def __str__(self):
-        return self.name
+        return self.user.phone
+    
+    class Meta:
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
+        
 
     @classmethod
     def _create_fake(self):
@@ -22,10 +65,22 @@ class OrderModel(AbstractBaseModel):
 
 
 class OrderitemModel(AbstractBaseModel):
-    name = models.CharField(_("name"), max_length=255)
+    order = models.ForeignKey(
+        OrderModel, 
+        on_delete=models.CASCADE,
+        related_name="orderitem",
+    )
+    book = models.ForeignKey(
+        "havasbook.BookModel",
+        on_delete=models.CASCADE,
+        related_name="orderitem"
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
+
 
     def __str__(self):
-        return self.name
+        return self.order.user.first_name
 
     @classmethod
     def _create_fake(self):
