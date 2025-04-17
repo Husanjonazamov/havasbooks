56eb42f765ec49c577b89521187ae9c2814938c6
HusanjonAzamov <azamovhusanboy08@gmail.com>
2025-04-17 16:33:40 +0500

    dded OrderFilter to filters and imported in __init__.py

--- core/apps/havasbook/filters/__init__.py
+++ core/apps/havasbook/filters/__init__.py
@@ -1,5 +1,6 @@
 from .banner import *  # noqa
 from .book import *  # noqa
+from .order import *  # noqa
 from .preorder import *  # noqa
 from .products import *  # noqa
 from .variants import *  # noqa
