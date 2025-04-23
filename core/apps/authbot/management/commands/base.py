a87bfaed6dc14e4d1b3b3e495c7cf6e68b3874ec
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-23 16:44:23 +0500

    ` "authbot modulini o'chirib tashlandi"`]

--- core/apps/authbot/management/commands/base.py
+++
@@ -1,5 +0,0 @@
-from telebot import TeleBot
-from config.env import env
-
-
-bot = TeleBot(env.str("BOT_TOKEN"))
--- core/apps/authbot/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import LoginView
-from django.urls import path
-
-
-app_name = "authbot"
-
-urlpatterns = [path("login/", LoginView.as_view(), name="login")]
