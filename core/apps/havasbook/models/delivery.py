134e5195acd12085592f6d951ea43eaddf6d2210
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-23 17:01:37 +0500

    DeliveryModel: price maydonini qo'shdi, OrderModel: payment_method ni o'zgartirdi

--- core/apps/havasbook/models/delivery.py
+++ core/apps/havasbook/models/delivery.py
@@ -5,6 +5,7 @@ from django_core.models import AbstractBaseModel
 
 class DeliveryModel(AbstractBaseModel):
     title = models.CharField(_("Nomi"), max_length=255)
+    price = models.CharField(_("Narxi"), max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.title
