76e2c37ca6ffa2b35c5b2e87c017b0c2c982960b
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-29 11:51:31 +0500

    Bot ishga tushirish funksiyasini qo'shildi va muhim o'zgarishlar kiritildi

--- core/apps/bot/management/commands/bot.py
+++ core/apps/bot/management/commands/bot.py
@@ -1,22 +1,28 @@
 import telebot
 from telebot import types
 from config.env import env
+from django.core.management.base import BaseCommand
 
+import logging
 
 BOT_TOKEN = env.str('BOT_TOKEN')
+ADMIN_ID = env.int('ADMIN_ID')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+logging.basicConfig(level=logging.INFO)
 
-
-
-@bot.message_handler(commands=['start'])
-def start_handler(message: types.Message):
-    bot.send_message(chat_id=message.from_user.id, text="salom")
+class Command(BaseCommand):
+    help="Botni ishga tushirish"
     
-    
-
-bot.polling()
+    def handle(self, *args, **options):
+        bot.send_message(ADMIN_ID, text="bot ishga tushdi")
+        @bot.message_handler(commands=['start'])
+        def start_handler(message: types.Message):
+            bot.send_message(chat_id=message.from_user.id, text="salom")
+            
+        self.stdout.write("bot ishga tushirildi")
+        bot.polling()
 
 
 
