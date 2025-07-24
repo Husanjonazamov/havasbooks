8f453aa43431397ff1a91b523b4bb8a64d52a08e
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-07-24 12:56:21 +0500

    New g4f version: 0.5.7.5 (current: 0.3.9.7) | pip install -U g4f

--- bot.py
+++ bot.py
@@ -1,22 +1,22 @@
-import asyncio
-import logging
-from aiogram import Bot
-from config.env import env
-from loader import dp, bot
-from handler import register_all_handlers 
+# import asyncio
+# import logging
+# from aiogram import Bot
+# from config.env import env
+# from loader import dp, bot
+# from handler import register_all_handlers 
 
-ADMIN_ID = env.int("ADMIN_ID")
+# ADMIN_ID = env.int("ADMIN_ID")
 
-async def on_startup(bot: Bot):
-    try:
-        await bot.send_message(ADMIN_ID, "🤖 Bot ishga tushdi ✅")
-    except Exception as e:
-        logging.error(f"Botni ishga tushirishda xatolik: {e}")
+# async def on_startup(bot: Bot):
+#     try:
+#         await bot.send_message(ADMIN_ID, "🤖 Bot ishga tushdi ✅")
+#     except Exception as e:
+#         logging.error(f"Botni ishga tushirishda xatolik: {e}")
 
-async def main():
-    register_all_handlers(dp)      
-    await on_startup(bot)          
-    await dp.start_polling(bot)   
+# async def main():
+#     register_all_handlers(dp)      
+#     await on_startup(bot)          
+#     await dp.start_polling(bot)   
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     asyncio.run(main())
